dae81b451d56e770fee43fd73b54865694b606a6
Mr Josh <mr-josh@sparklapse.com>
2023-02-01 16:23:07 +1100

    Added a code setup file
    This setup installs some extensions I use regularly

---
+++ templates/code-space/image/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Setup for a remote VSCode Desktop session
+# These are common extensions I use in all projects
+
+# Generic
+code --install-extension github.copilot
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension exodiusstudios.comment-anchors
+code --install-extension tyriar.sort-lines
+
+# Web dev
+code --install-extension esbenp.prettier-vscode
+code --install-extension wallabyjs.quokka-vscode
+
+# Python
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.isort
